c230098759fb73c0c8f7b5b330f9368055ab006c
Bruner <bruner.expert@yandex.ru>
2022-10-23 16:26:49 +0300

    create form

--- mysite/news/forms.py
+++ mysite/news/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Category
 
 class NewsForm(forms.Form):
-    title = forms.CharField(max_length=150, label='Название')
-    content = forms.CharField(label='Текст', required=False)
+    title = forms.CharField(max_length=150, label='Название', widget=forms.TextInput(attrs={'class': 'form-control'})) #тут мы подключаем определенный виджет поля и шаблон класса из бустрапа чтобы изменить внешний вид поля
+    content = forms.CharField(label='Текст', required=False, widget=forms.Textarea(attrs={'rows': 5,'class': 'form-control'}))
     is_published = forms.BooleanField(label='Опубликовано', initial=True)
-    category = forms.ModelChoiceField(queryset=Category.objects.all(), label='Катеригия', empty_label='Выберете категорию')
+    category = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категория', empty_label='Выберете категорию', widget=forms.Select(attrs={'class': 'form-control'}))
